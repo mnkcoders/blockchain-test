17b9b18b6b9db59875ba5a2394fbb143eaf9a80c
mnkcoders <mnkcoders@gmail.com>
2022-08-11 10:41:11 +0200

    Added http server ...

---
+++ main.backup.js
@@ -0,0 +1,9 @@
+
+const Account = require('./lib/account');
+const Block = require('./lib/block');
+const Chain = require('./lib/chain');
+const HASH = require('./lib/hash');
+const Node = require('./lib/node');
+
+
+console.log( new Chain.Chain() );
